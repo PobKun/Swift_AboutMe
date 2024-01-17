668dd4e021c4c036d54ffab6457e0e90073e90ba
PobKun <pop.sri00@gmail.com>
2024-01-17 14:10:16 +0700

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Pattaraphon on 17/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AboutMe
+//
+//  Created by Pattaraphon on 17/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
